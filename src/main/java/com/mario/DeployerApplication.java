1f392833089cd3fbebbe63ddb14d426532e9fb12
Mariusz Pawlowski <mariusz.pawlowski@dynatrace.com>
2015-11-19 09:54:42 +0100

    auth code

--- src/main/java/com/mario/DeployerApplication.java
+++ src/main/java/com/mario/DeployerApplication.java
@@ -2,15 +2,16 @@ package com.mario;
 
 import com.mariuszpawlowski.tiktalik.TiktalikJava;
 import com.mariuszpawlowski.tiktalik.TiktalikJavaImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @SpringBootApplication
 public class DeployerApplication implements CommandLineRunner {
+
+    @Value("${authCodeTiktalik}")
+    String authCodeTiktalik;
 
     public static void main(String[] args) {
         SpringApplication.run(DeployerApplication.class, args);
@@ -18,8 +19,8 @@ public class DeployerApplication implements CommandLineRunner {
 
     @Override
     public void run(String... strings) throws Exception {
-        TiktalikJava tiktalikJava = new TiktalikJavaImpl("");
-
+        TiktalikJava tiktalikJava = new TiktalikJavaImpl(authCodeTiktalik);
+        tiktalikJava.getListOfInstances();
 
     }
 
