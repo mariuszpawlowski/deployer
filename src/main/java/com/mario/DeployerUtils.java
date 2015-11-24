5c5664fe9042c5e870bd80ddcdbb44ce16c6f97b
Mariusz Pawlowski <mario@data.pl>
2015-11-24 23:27:45 +0100

    small improvements

--- src/main/java/com/mario/DeployerUtils.java
+++ src/main/java/com/mario/DeployerUtils.java
@@ -4,6 +4,7 @@ import com.mariuszpawlowski.tiktalik.entity.Instance;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.jar.Pack200;
 
 /**
  * Created by Mariusz.Pawlowski on 2015-11-24.
@@ -26,17 +27,37 @@ public class DeployerUtils {
         return vpsUuid;
     }
 
+    public static boolean checkIfTeamCityInstanceIsRunning(List<Instance> instances, String hostName) {
+        boolean teamcityInstanceIsRunning = false;
+        Optional<Instance> instance = instances.stream()
+                .filter(i -> i.getHostname().equals(hostName))
+                .findFirst();
+
+        if (instance.isPresent()){
+            if (instance.get().getRunning()){
+                teamcityInstanceIsRunning = true;
+                System.out.println("Teamcity server instance is running.");
+            } else {
+                System.out.println("Teamcity server instance is not running.");
+            }
+        } else {
+            System.out.println("Teamcity server instance does not exist.");
+        }
+
+        return teamcityInstanceIsRunning;
+    }
+
     public static boolean checkIfTeamCityInstanceExists(List<Instance> instances, String hostName) {
         boolean teamcityInstanceExists = false;
         Optional<Instance> instance = instances.stream()
                 .filter(i -> i.getHostname().equals(hostName))
                 .findFirst();
 
-        if (instance.get().getRunning()){
+        if (instance.isPresent()){
+            System.out.println("Teamcity server instance exists.");
             teamcityInstanceExists = true;
-            System.out.println("Teamcity server instance is running.");
         } else {
-            System.out.println("Teamcity server instance is not running.");
+            System.out.println("Teamcity server instance does not exist.");
         }
 
         return teamcityInstanceExists;
